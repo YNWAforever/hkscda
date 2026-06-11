3f490646f72d44139723acbc90cdbdc7c07e1265
Willy Lai <laichiwillyjp@gmail.com>
2026-06-12 03:28:26 +0800

    fix: GA4 inline script uses window.gtag for analytics.ts compatibility

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -136,7 +136,7 @@ function RootShell({ children }: { children: ReactNode }) {
       <body>
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(){var m='G-XXXXXXXXXX';var s=document.createElement('script');s.src='https://www.googletagmanager.com/gtag/js?id='+m;s.async=true;document.head.appendChild(s);window.dataLayer=window.dataLayer||[];function gtag(){window.dataLayer.push(arguments);}gtag('js',new Date());gtag('config',m);})()`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','G-XXXXXXXXXX');window.gtag=function(){window.dataLayer.push(arguments);};window.gtag('js',new Date());window.gtag('config','G-XXXXXXXXXX')`,
           }}
         />
         <a
